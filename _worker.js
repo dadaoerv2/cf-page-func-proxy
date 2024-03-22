525711e1b595e5fd99c0579336eb06d44a27b560
jason <81965307+dadaoerv2@users.noreply.github.com>
2024-03-22 11:11:58 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="xn--tv-t22c68s.me66.me";
+        url.hostname="nave.chat4free.workers.dev";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
